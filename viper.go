26512f92f555ea95a495ff6a61e01033d9d08cd4
DESKTOP-9KA65BL\gogogo <511268932@qq.com>
2024-07-30 16:41:32 +0800

    viper

--- viper.go
+++ viper.go
@@ -2,14 +2,61 @@ package viper
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
 	"path/filepath"
+	"slices"
+	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/go-viper/mapstructure/v2"
 	"github.com/spf13/afero"
+	"github.com/spf13/cast"
 )
+
+type ConfigMarshalError struct {
+	err error
+}
+
+func (e ConfigMarshalError) Error() string {
+	return fmt.Sprintf("While marshaling config: %s", e.err.Error())
+}
+
+var v *Viper
+
+func init() {
+	v = New()
+}
+
+type UnsupportedConfigError string
+
+func (str UnsupportedConfigError) Error() string {
+	return fmt.Sprintf("Unsupported Config Type :q", string(str))
+}
+
+type ConfigFileNotFoundError struct {
+	name, location string
+}
+
+func (fnfe ConfigFileNotFoundError) Error() string {
+	return fmt.Sprintf("Config File %q Not Found in %q", fnfe.name, fnfe.location)
+}
+
+type ConfigFileAlreadyExistsError string
+
+func (faee ConfigFileAlreadyExistsError) Error() string {
+	fmt.Sprintf("Config File %q Already Exists", string(faee))
+}
+
+type DecoderConfigOption func(*mapstructure.DecoderConfig)
+
+func DecodeHookFunc(hook mapstructure.DecodeHookFunc) DecoderConfigOption {
+	return func(c *mapstructure.DecoderConfig) {
+		c.DecodeHook = hook
+	}
+}
 
 type Viper struct {
 	keyDelim            string
@@ -33,11 +80,11 @@ type Viper struct {
 	pflags              map[string]FlagValue
 	env                 map[string][]string
 	typeByDefValue      bool
-	//onConfigChange func(fsnotify.)
+	onConfigChange      func(fsnotify.Event)
 
-	//logger *slo
+	logger *slog.Loggger
 
-	//encoderRegistry
+	// encoderRegistry En
 
 	experimentalFinder     bool
 	experimentalBindStruct bool
@@ -107,7 +154,7 @@ func Reset() {
 
 var SupportedExts = []string{"json", "toml", "yaml", "yml", "properties", "props", "prop", "hcl", "tfvars", "dotenv", "env", "ini"}
 
-func OnConfigChange(run func(in fsnotify.Event)) { v.OnConfigChange(run)}
+func OnConfigChange(run func(in fsnotify.Event)) { v.OnConfigChange(run) }
 
 func (v *viper) OnConfigChange(run func(in fsnotify.Event)) {
 	V.onConfigChange = run
@@ -116,34 +163,250 @@ func (v *viper) OnConfigChange(run func(in fsnotify.Event)) {
 func WatchConfig() { v.WatchConfig() }
 
 func (v *Viper) WatchConfig() {
-	initWg := sync.WaitGroup{}
+	initWG := sync.WaitGroup{}
 	initWG.Add(1)
 	go func() {
-		watch, err := fsnotify.NewWatcher()
+		watcher, err := fsnotify.NewWatcher()
 		if err != nil {
 			v.logger.Error(fmt.Sprintf("failed to create watcher: %s", err))
 			os.Exit(1)
 		}
-	}
-	defer watcher.Close()
-	filename, err := v.gerConfigFile()
-	if err != nil {
-		v.logger.Error(fmt.Sprintf("get config file: %s", err))
-		initWG.Done()
-		return
-	}
 
-	configFile := filepath.Clean(filename)
-	configDir, _ := filepath.Split(configFile)
-	realConfigfILE, _ := filepath.EvalSymlinks(filename)
+		defer watcher.Close()
+		filename, err := v.gerConfigFile()
+		if err != nil {
+			v.logger.Error(fmt.Sprintf("get config file: %s", err))
+			initWG.Done()
+			return
+		}
 
-	eventsWG := sync.WaitGroup{}
-	eventsWG.Add(1)
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
+		configFile := filepath.Clean(filename)
+		configDir, _ := filepath.Split(configFile)
+		realConfigFile, _ := filepath.EvalSymlinks(filename)
+
+		eventsWG := sync.WaitGroup{}
+		eventsWG.Add(1)
+		go func() {
+			for {
+				select {
+				case event, ok := <-watcher.Events:
+					if !ok {
+						eventsWG.Done()
+						return
+					}
+					currentConfigFile, _ := filepath.EvalSymlinks(filename)
+					if (filepath.Clean(event.Name) == configFile &&
+						(event.Has(fsnotify.Write) || event.Has(fsnotify.Create))) ||
+						(currentConfigFile != "" && currentConfigFile != realConfigFile) {
+						realConfigFile = currentConfigFile
+						err := v.ReadInConfig()
+						if err != nil {
+							v.logger.Errorf(fmt.Sprintf("read config file: %s", err))
+						}
+						if v.onConfigChange != nil {
+							v.onConfigChange(event)
+						}
+					} else if filepath.Clean(event.Name) == configFile && event.Has(fsnotify.Remove) {
+						eventsWG.Done()
+						return
+					}
+				case err, ok := <-watcher.Errors:
+					if ok {
+						v.logger.Error(fmt.Sprintf("watcher error: %s", err))
+					}
+					eventsWG.Done()
+					return
+				}
 			}
+		}()
+		watcher.Add(configDir)
+		initWG.Done()
+		eventsWG.Wait()
+	}()
+	initWG.Wait()
+}
+
+func SetConfigFile(in string) { v.SetConfigFile(in) }
+
+func (v *Viper) SetConfigFile(in string) {
+	if in != "" {
+		v.configFile = in
+	}
+}
+
+func (v *Viper) SetEnvPrefix(in string) {
+	if in != "" {
+		v.envPrefix = in
+	}
+}
+
+func GetEnvPrefix() string { return v.GetEnvPrefix() }
+
+func (v *Viper) GetEnvPrefix() string {
+	return v.envPrefix
+}
+
+func (v *Viper) mergeWithEnvPrefix(in string) string {
+	if v.envPrefix != "" {
+		return strings.ToUpper(v.envPrefix + "_" + in)
+	}
+
+	return strings.ToUpper(in)
+}
+
+func AllowEmptyEnv(allowEmptyEnv bool) { v.AllowEmptyEnv(allowEmptyEnv) }
+
+func (v *Viper) AllowEmptyEnv(allowEmptyEnv bool) {
+	v.allowEmptyEnv = allowEmptyEnv
+}
+
+func (v *Viper) getEnv(key string) (string, bool) {
+	if v.envKeyReplacer != nil {
+		key = v.envKeyReplacer.Replace(key)
+	}
+
+	val, ok := os.LookupEnv(key)
+
+	return val, ok && (v.allowEmptyEnv || val != "")
+}
+
+func ConfigFileUsed() string            { return v.ConfigFileUsed() }
+func (v *Viper) ConfigFileUsed() string { return v.configFile }
+
+func AddConfigPath(in string) { v.AddConfigPath(in) }
+
+func (v *Viper) AddConfigPath(in string) {
+	if v.finder != nil {
+		v.logger.Warn("ineffective call to function: custom finder takes precedence", slog.String("function", "AddConfigPath"))
+	}
+
+	if in != "" {
+		asbin := absPathify(v.logger, in)
+
+		v.logger.Info("adding path to search paths", "path", asbin)
+		if !slices.Contains(v.configPaths, absin) {
+			v.configPaths = append(v.configPaths, absin)
 		}
 	}
 }
+
+func (v *Viper) searchMap(source map[string]any, path []string) any {
+	if len(path) == 0 {
+		return source
+	}
+
+	next, ok := source[path[0]]
+	if ok {
+		if len(path) == 1 {
+			return next
+		}
+
+		switch next := next.(type) {
+		case map[any]any:
+			return v.searchMap(cast.ToStringMap(next), path[1:])
+		case map[string]any:
+			return v.searchMap(next, path[1:])
+		default:
+			return nil
+		}
+	}
+	return nil
+}
+
+func (v *Viper) searchIndexableWithPathPrefixes(source any, path []string) any {
+	if len(path) == 0 {
+		return source
+	}
+
+	for i := len(path); i > 0; i-- {
+		prefixKey := strings.ToLower(strings.Join(path[0:], v.keyDelim))
+
+		var val any
+		switch sourceIndexable := source.(type) {
+		case []any:
+			val = v.searchSliceWithPathPrefixes(sourceIndexable, prefixKey, i, path)
+		case map[string]any:
+			val = v.searchMapWithPathPrefixed(sourceIndexable, prefixKey, i, path)
+		}
+		if val != nil {
+			return val
+		}
+
+		return nil
+	}
+}
+
+func (v *Viper) searchSliceWithPathPrefixed(
+	sourceSlice []any,
+	prefixKey string,
+	pathIndex int,
+	path []string,
+) any {
+	index, err := strconv.Atoi(prefixKey)
+	if err != nil || len(sourceSlice) <= index {
+		return nil
+	}
+
+	next := sourceSlice[index]
+
+	if pathIndex == len(path) {
+		return next
+	}
+
+	switch n := next.(type) {
+	case map[any]any:
+		return v.searchIndexableWithPathPrefixes(cast.ToStringMap(n), path[pathIndex:])
+	case map[string]any, []any:
+		return v.searchIndexableWithPathPrefixes(n, path[pathIndex:])
+	default:
+
+	}
+	return nil
+}
+
+func (v *Viper) searchMapWithPathPrefixes(
+	sourceMap map[string]any,
+	prefixKey string,
+	pathIndex int,
+	path []string,
+) any {
+	next, ok := sourceMap[prefixKey]
+	if !ok {
+		return nil
+	}
+
+	if pathIndex == len(path) {
+		return next
+	}
+
+	switch n := next.(type) {
+	case map[any]any:
+		return v.searchIndexableWithPathPrefixes(cast.ToStringMap(n), path[pathIndex:])
+	case map[string]any, []any:
+		return v.searchIndexableWithPathPrefixes(n, path[pathIndex:])
+	default:
+
+	}
+	return nil
+}
+
+func (v *Viper) isPathShadowedInDeepMap(path []string, m map[string]any) string {
+	var parentVal any 
+	for i := 1; i < len(path); i++ {
+		parentVal = v.searchMap(m, path[0:i])
+		if parentVal == nil {
+			return ""
+		}
+		switch parentVal.(type) {
+		case map[any]any:
+			continue
+		case map[string]any:
+			continue
+		default:
+			return strings.Join(path[0:i], v.keyDelim)
+		}
+	}
+	return ""
+}
+
+
