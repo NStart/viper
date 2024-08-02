2316ae86a7128893a636c83fbe46a45a49fc57d6
DESKTOP-9KA65BL\gogogo <511268932@qq.com>
2024-08-02 10:16:48 +0800

    flag

--- flags.go
+++ flags.go
@@ -1,6 +1,9 @@
 package viper
 
+import "github.com/spf13/pflag"
+
 type FlagValueSet interface {
+	VisitAll(fn func(FlagValue))
 }
 
 type FlagValue interface {
@@ -8,4 +11,34 @@ type FlagValue interface {
 	Name() string
 	ValueString() string
 	ValueType() string
+}
+
+type pflagValueSet struct {
+	flags *pflag.FlagSet
+}
+
+func (p pflagValueSet) VisitAll(fn func(flag FlagValue)) {
+	p.flags.VisitAll(func(flag *pflag.Flag) {
+		fn(pflagValue{flag})
+	})
+}
+
+type pflagValue struct {
+	flag *pflag.Flag
+}
+
+func (p pflagValue) HasChanged() bool {
+	return p.flag.Changed
+}
+
+func (p pflagValue) Name() string {
+	return p.flag.Name
+}
+
+func (p pflagValue) ValueString() string {
+	return p.flag.Value.String()
+}
+
+func (p pflagValue) ValueType() string {
+	return p.flag.Value.Type()
 }
